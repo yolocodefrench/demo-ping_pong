7f36bb3dc586279890a0253a69cade99c609cd3a
pierre chene <pierre.chene44@gmail.com>
2018-06-05 18:26:23 +0200

    ajout des fichiers

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-PingPong.get("https://talaikis.com/api/quotes/random/", function(data){
-	data = JSON.parse(data);
-	data = {"datas":{'message':data}};
-	console.log(data);
-	PingPong.render("example_pres", "./templates/template.html", data)
-})
+render();
+
+document.getElementById("mybtn").addEventListener("click", function(){
+    render();
+});
+
+function render(){
+	PingPong.get("https://talaikis.com/api/quotes/random/", function(data){
+		data = JSON.parse(data);
+		data = {"datas":{'message':data}};
+		console.log(data);
+		PingPong.render("example_pres", "./templates/template.html", data)
+	});
+}
